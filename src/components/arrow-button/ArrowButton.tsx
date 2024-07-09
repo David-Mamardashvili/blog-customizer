836bed1d89790b6bdbf817cff65e3f96ebf3c26c
David-Mamardashvili <144165614+David-Mamardashvili@users.noreply.github.com>
2024-07-09 23:57:30 +0300

    first commit

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -5,15 +5,21 @@ import styles from './ArrowButton.module.scss';
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+interface IArrowButtonProps {
+	onClick?: OnClick;
+	isOpen?: boolean;
+}
+
+export const ArrowButton = (props: IArrowButtonProps) => {
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={`${styles.container} ${props.isOpen ? styles.container_open : ""}`}
+			onClick={props.onClick}>
+			<img src={arrow} alt='иконка стрелочки' className={`${styles.arrow} ${props.isOpen ? styles.arrow_open : ""}` } />
 		</div>
 	);
 };
